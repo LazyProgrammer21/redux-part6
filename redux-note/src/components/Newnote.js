ff2cd50f8ee38970b7a950ae88c62410629c532f
Lazyprogrammer21 <tamang.subash36@gmail.com>
2022-08-24 16:03:53 +0545

    feat: Asynchronous actions and redux-thunk

--- redux-note/src/components/Newnote.js
+++ redux-note/src/components/Newnote.js
@@ -1,20 +1,30 @@
 import { useDispatch } from "react-redux";
 import { createNote } from "../reducers/noteReducer";
-import noteService from "../services/notes";
+
 const Newnote = () => {
   const dispatch = useDispatch();
   const addNote = async (event) => {
     event.preventDefault();
-
     const content = event.target.note.value;
     if (content === "") {
       alert("Notes cannot be Empty");
     } else {
       event.target.note.value = "";
-      const newNote = await noteService.createNew(content);
-      dispatch(createNote(newNote));
+      dispatch(createNote(content));
     }
   };
+  // const addNote = async (event) => {
+  //   event.preventDefault();
+
+  //   const content = event.target.note.value;
+  //   if (content === "") {
+  //     alert("Notes cannot be Empty");
+  //   } else {
+  //     event.target.note.value = "";
+  //     const newNote = await noteService.createNew(content);
+  //     dispatch(createNote(newNote));
+  //   }
+  // };
   return (
     <>
       <form onSubmit={addNote}>
--- redux-note/src/index.js
+++ redux-note/src/index.js
@@ -2,21 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import filterReducer from "./reducers/filterReducer";
-import { configureStore } from "@reduxjs/toolkit";
-import noteService from "./services/notes";
-import noteReducer, { setNotes } from "./reducers/noteReducer";
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-noteService.getAll().then((notes) =>
-  notes.forEach((note) => {
-    store.dispatch(setNotes(notes));
-  })
-);
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
